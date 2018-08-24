499d31a1e5d74e1dee5af65a9fe4afa77b5d28f5
alejandra <alesquerro@hotmail.com>
2018-08-24 12:46:52 -0300

    rompi el nav cuando pase todos los links de css y js a local

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -25,6 +25,9 @@
         </div>
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> --}}
+    <script src="js/jQuery/jquery.min.js"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script> --}}
+    <script src="js/jQuery/popper.min.js"></script>
+    {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script> --}}
+    <script src="js/jQuery/bootstrap.min.js"></script>
--- resources/views/Components/head.blade.php
+++ resources/views/Components/head.blade.php
@@ -2,17 +2,36 @@
   <title>Armá tu evento</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> <!-- ICONS -->
+  
+
+  {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"> --}}
+  <link rel="stylesheet" href="/css/Bootstrap/bootstrap.min.css">
+  
+
+{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> --}}
+  <link rel="stylesheet" href="public/css/Bootstrap/font-awesome.min.css">
+
+
   <link rel="stylesheet" type="text/css" href="/css/styles.css">
   <meta name="keywords" content="evento, servicios, catering">
   <meta name="description" content="Web para armar tu evento">
   <meta name="author" content="Ale, Roma, Flora, Dani">
   <link rel="icon" type="image/png" href="imagenes/logoPequeno.png"/>
-  <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet"> <!-- FONTS -->
-  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> <!-- FONTS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+
+  {{-- <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">  FONTS  --}}
+  <link href="public/fonts/familyRobotoSlab" rel="stylesheet"> <!-- FONTS -->
+
+
+{{-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">  FONTS  --}}
+  <link href="public/fonts/familyRoboto" rel="stylesheet"> <!-- FONTS -->
+
+
+  {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
+  <link rel="stylesheet" href="public/css/Bootstrap/font-awesomeB.min.css">
+
+  {{-- <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script> --}}
+  <script src="js/jQuery/jquery-3.1.1.min.js"></script>
   <script src="{{ asset('js/share.js') }}"></script>
 </head>
